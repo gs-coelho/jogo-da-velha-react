fa66302a31f92bbfe49af7de0d9b5760c410e9c4
Gabriel Coelho <coelho.gabrielcs@gmail.com>
2023-01-28 23:52:37 -0300

    Concluindo a funcionalidade base do jogo
    O jogo agora alterna entre os jogadores, e também mostra o vencedor.

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,28 @@ import { useState } from "react";
 import { Square, SquareValue } from "./components/Square";
 
 export default function Board() {
+  const [isXTurn, setIsXTurn] = useState(true);
   const [squares, setSquares] = useState(Array(9).fill(null) as SquareValue[]);
 
+  const winner = calculateWinner(squares);
+  const status = winner ? 
+    `O vencedor é ${winner}!` :
+    `É a vez de ${isXTurn ? "X" : "O"}`;
+
   function handleClick(i: number): void {
+    if(squares[i] || winner) return;
+
     const newSquares = squares.slice();
-    newSquares[i] = "X" as SquareValue;
+    newSquares[i] = isXTurn ? "X" : "O";
+
+    setIsXTurn(!isXTurn);
     setSquares(newSquares);
   }
 
+
   return(
     <>
+      <div className="status">{status}</div>
       <div className="board-row">
         <Square value={squares[0]} onSquareClick={() => handleClick(0)}/>
         <Square value={squares[1]} onSquareClick={() => handleClick(1)}/>
@@ -29,4 +41,25 @@ export default function Board() {
       </div>
     </>
   );
+}
+
+function calculateWinner(squares: SquareValue[]): SquareValue{
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+
+  for (const [a, b, c] of lines) {
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+
+  return null;
 }
