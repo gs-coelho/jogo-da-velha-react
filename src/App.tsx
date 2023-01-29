7b797b29ca7a75c9f6cf88b96768a04125025914
Gabriel Coelho <coelho.gabrielcs@gmail.com>
2023-01-29 01:10:34 -0300

    Adicionando a navegação do histórico de jogadas

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,39 @@ import { Board } from "./components/Board";
 import { SquareValue } from "./components/Square";
 
 export default function Game() {
-  const [isXTurn, setIsXTurn] = useState(true);
   const [history, setHistory] = useState([Array(9).fill(null) as SquareValue[]]);
-  const currentSquares = history[history.length - 1];
+  const [currentMove, setCurrentMove] = useState(0);
+  const isXTurn = currentMove % 2 === 0;
+  const currentSquares = history[currentMove];
 
   function handlePlay(nextSquares: SquareValue[]){
-    setIsXTurn(!isXTurn);
-    setHistory([...history, nextSquares]);
+    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+    setHistory(nextHistory);
+    setCurrentMove(nextHistory.length - 1)
   }
+
+  function jumpTo(nextMove: number){
+    setCurrentMove(nextMove);
+  }
+
+  const moves = history.map((squares: SquareValue[], moveIndex: number) => {
+    let description;
+
+    if(moveIndex === 0){
+      description = `Voltar para o início do jogo`
+    } else if (moveIndex === currentMove) {
+      description = `Você está na jogada ${moveIndex}`
+    } else {
+      description = `Voltar para a jogada ${moveIndex}`;
+    }
+    return (
+      <li key={moveIndex}>
+        <button onClick={() => jumpTo(moveIndex)} disabled={moveIndex === currentMove}>
+          {description}
+        </button>
+      </li>
+    );
+  });
 
   return (
     <div className="game">
@@ -18,7 +43,7 @@ export default function Game() {
         <Board isXTurn={isXTurn} squares={currentSquares} onPlay={handlePlay}/>
       </div>
       <div className="game-info">
-        <ol></ol>
+        <ul>{moves}</ul>
       </div>
     </div>
   )
